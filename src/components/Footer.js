290df639e96a35767add64e37de1061cf57dfc92
Matt Schwarz <mattschwarz5@gmail.com>
2022-04-22 16:01:52 -0500

    Fixed footer icons spacing on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,12 @@ import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 export default function Footer() {
     return (
         <section id="footer" className="bg-gray-800">
-            <div className="container mx-auto flex flex-wrap p-10 flex-col md:flex-row items-center justify-center">
+            <div className="container mx-auto flex flex-wrap p-10 md:flex-row items-center justify-center space-x-5">
             <a
                 href="https://www.linkedin.com/in/matt-schwarz/"
                 target="_blank"
                 rel="noopener noreferrer">
-                <FaLinkedin className="text-islamic-green text-4xl mr-5" />
+                <FaLinkedin className="text-islamic-green text-4xl" />
             </a>
             <a
                 href="https://github.com/mjschwarz"
