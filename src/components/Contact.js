68cfea99cd100345dff02ac0240c847ad5d45511
Matt Schwarz <mattschwarz5@gmail.com>
2023-11-29 08:45:58 -0600

    Modified Contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,14 +39,15 @@ export default function Contact() {
             style={{ filter: "opacity(0.7)" }}
             src="https://www.google.com/maps/embed/v1/place?q=2201+Vanderbilt+Place,+Nashville,+TN,+32735&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
+          /*
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 ADDRESS
               </h2>
               <p className="mt-1">
-                2201 West End Ave  <br />
-                Nashville, TN 37235
+                FIXME: Street address here  <br />
+                FIXME: City/state/zip here
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -54,14 +55,15 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-malachite leading-relaxed pr-6" href="mailto:mattschwarz5@gmail.com">
-                mattschwarz5@gmail.com
+                FIXME: Email here
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">704-775-2546</p>
+              <p className="leading-relaxed">FIXME: Phone # here</p>
             </div>
           </div>
+          */
         </div>
         <form
           netlify
